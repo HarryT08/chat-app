0b24e47f08e3f83873c05224d652d223fbcff5fe
Felipe Alferez <alferezfelipe@gmail.com>
2023-05-31 17:27:34 -0500

    add profile user

--- app/src/main/java/com/example/chat_app/SetupProfileActivity.kt
+++ app/src/main/java/com/example/chat_app/SetupProfileActivity.kt
@@ -1,11 +1,116 @@
 package com.example.chat_app
 
+import android.app.ProgressDialog
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.chat_app.databinding.ActivitySetupProfileBinding
+import com.example.chat_app.model.User
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
 
 class SetupProfileActivity : AppCompatActivity() {
+    var binding:ActivitySetupProfileBinding? = null
+    var auth:FirebaseAuth? = null
+    var database: FirebaseDatabase? = null
+    var storage: FirebaseStorage? = null
+    var selectedImage : Uri? = null
+    var dialog: ProgressDialog? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_setup_profile)
+        binding = ActivitySetupProfileBinding.inflate(layoutInflater)
+        setContentView(binding!!.root)
+        dialog!!.setMessage("Updating Profile...")
+        dialog!!.setCancelable(false)
+        database = FirebaseDatabase.getInstance()
+        storage = FirebaseStorage.getInstance()
+        auth = FirebaseAuth.getInstance()
+        supportActionBar?.hide()
+        binding!!.imageUser.setOnClickListener {
+            val intent = Intent()
+            intent.action = Intent.ACTION_GET_CONTENT
+            intent.type = "image/*"
+            startActivityForResult(intent, 45)
+        }
+        binding!!.profileContinueBtn.setOnClickListener {
+
+            val name : String  = binding!!.profileName.text.toString()
+            val lastname : String  = binding!!.profileLastname.text.toString()
+            val username : String = binding!!.username.text.toString()
+            val email : String = binding!!.email.text.toString()
+
+            if(name.isEmpty()) {
+                binding!!.profileName.setError("Porfavor ingresa un nombre")
+            }
+
+            if(lastname.isEmpty()) {
+                binding!!.profileLastname.setError("Porfavor ingresa un apellido")
+            }
+
+            if(username.isEmpty()) {
+                binding!!.username.setError("Porfavor ingresa un nombre de usuario")
+            }
+
+            if(email.isEmpty()) {
+                binding!!.email.setError("Porfavor ingresa un email")
+            }
+
+            dialog!!.show()
+            if(selectedImage != null){
+                val reference = storage!!.reference.child("Profile").child(auth!!.uid!!)
+                reference.putFile(selectedImage!!).addOnCompleteListener { task ->
+                    if(task.isSuccessful){
+                        reference.downloadUrl.addOnCompleteListener {uri ->
+                            val imageUrl = uri.toString()
+                            val uid = auth!!.uid
+
+                            val name : String  = binding!!.profileName.text.toString()
+                            val lastname : String  = binding!!.profileLastname.text.toString()
+                            val username : String = binding!!.username.text.toString()
+                            val email : String = binding!!.email.text.toString()
+                            val phoneNumber = auth!!.currentUser!!.phoneNumber
+
+                            val user = User(uid,  name, username, lastname, phoneNumber, imageUrl, email)
+                            database!!.reference
+                                .child("users")
+                                .child(uid!!)
+                                .setValue(user)
+                                .addOnCompleteListener {
+                                    dialog!!.dismiss()
+                                    val intent = Intent(this@SetupProfileActivity, MainActivity::class.java)
+                                    startActivity(intent)
+                                    finish()
+                                }
+                        }
+                    }
+                    else {
+                        val uid = auth!!.uid
+                        val phoneNumber = auth!!.currentUser!!.phoneNumber
+                        val name : String  = binding!!.profileName.text.toString()
+                        val lastname : String  = binding!!.profileLastname.text.toString()
+                        val username : String = binding!!.username.text.toString()
+                        val email : String = binding!!.email.text.toString()
+
+                        val user = User(uid, name, username, lastname, phoneNumber, "No image", email)
+                        database!!.reference
+                            .child("users")
+                            .child(uid!!)
+                            .setValue(user)
+                            .addOnCanceledListener {
+                                dialog!!.dismiss()
+                                val intent = Intent(this@SetupProfileActivity, MainActivity::class.java)
+                                startActivity(intent)
+                                finish()
+                            }
+                    }
+                }
+            }
+
+
+        }
+
     }
 }
---
+++ app/src/main/java/com/example/chat_app/model/User.kt
@@ -0,0 +1,31 @@
+package com.example.chat_app.model
+
+class User {
+    var uid:String? = null
+    var name:String? = null
+    var lastname:String? = null
+    var phoneNumber : String? =  null
+    var email:String? = null
+    var username:String? = null
+    var profileImage:String? = null
+
+    constructor(){}
+    constructor(
+        uid:String?,
+        name:String?,
+        username:String?,
+        lastname:String?,
+        phoneNumber:String?,
+        profileImage:String?,
+        email:String?
+    ) {
+        this.uid = uid
+        this.name = name
+        this.username = username
+        this.lastname = lastname
+        this.phoneNumber = phoneNumber
+        this.profileImage = profileImage
+        this.email = email
+    }
+
+}
